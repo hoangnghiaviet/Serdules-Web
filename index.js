d13c66eb3e368483b3654e8173574aa717f1deec
nguyen ba cu <cuninjazas3@gmail.com>
2022-04-20 22:18:48 +0700

    update UI #2

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-setInterval(() => $('.carousel').carousel('next'), 3000)
+setInterval(() => $('.carousel').carousel('next'), 5000)
+
+
+lottie.loadAnimation({
+    container: document.getElementById('slide2_ani'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './assets/json/slide2.json' // the path to the animation json
+});
+
+lottie.loadAnimation({
+    container: document.getElementById('slide3_ani'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './assets/json/slide3.json' // the path to the animation json
+});
